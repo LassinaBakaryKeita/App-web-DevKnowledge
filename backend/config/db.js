58a6d58a811eefb715332d75d80e0e88f737e361
LassinaBakaryKeita <lassbakkeita@gmail.com>
2026-04-20 08:55:06 +0100

    changement dans le fichier db.js côté Backend

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,18 +1,24 @@
-const mongoose= require("mongoose");
-//require('dotenv').config({path:'./config/.env'});
+const mongoose = require('mongoose');
 
-const connectDB= async ()=>{
-    try{
-        const conn = await mongoose.connect(
-            process.env.MONGODB_URI
-        );
+let isConnected = false;
 
-        console.log("Connexion à  MongoDB Atlas reussie !!!");    
-    }catch(error){
-        console.log("Connexion à MongoDB Atlas echouée "+error);
+const connectDB = async () => {
+    // Si déjà connecté, on réutilise la connexion existante
+    if (isConnected) {
+        return;
+    }
+
+    try {
+        const db = await mongoose.connect(process.env.MONGODB_URI, {
+            serverSelectionTimeoutMS: 10000, // abandonne si pas connecté en 10s
+            socketTimeoutMS: 45000,          // timeout des opérations à 45s
+        });
+
+        isConnected = db.connections[0].readyState === 1;
+        console.log('Connexion à MongoDB Atlas réussie !');
+    } catch (error) {
+        console.log('Connexion à MongoDB Atlas échouée : ' + error);
     }
 };
 
-//export default connectDB;
-
-module.exports= connectDB;
+module.exports = connectDB;
